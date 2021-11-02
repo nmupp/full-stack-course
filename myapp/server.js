6c6a36363ee81253d8b01e55b5c6f023192434cf
Narasimhulu Muppalla <narsicloud@gmail.com>
2021-11-02 05:38:02 -0500

    cors

--- myapp/server.js
+++ myapp/server.js
@@ -2,15 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose');
 const movieRoutes = require('./movieRoutes')
 const userRoutes = require('./userRoutes')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = 3001
 
 //http://localhost:3000/movies - URI (POST)
 //http://localhost:3000/movies/:id - (PUT)
 //http://localhost:3000/movies - (GET)
 //http://localhost:3000/movies/:id - (GET)
 //http://localhost:3000/movies/:id - (DELETE)
-
+app.use(cors())
 app.use(express.json())
 
 app.use('/movies', movieRoutes)
